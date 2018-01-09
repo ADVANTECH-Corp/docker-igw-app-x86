937aca3694c6e894c0e03f56a22eb9ce47b1d0fb
Eric.Liang <eric.liang@advantech.com.tw>
2018-01-09 12:41:25 +0800

    [Edge Sense Service] add run bash for modbus service Date         : 2018/01/09 Bug #        : N/A Description  : add run bash for modbus service Files        : run_service-modbus.sh Reviewer     : Commit by    : Eric.Liang

---
+++ AgentService/run_service-modbus.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ServiceName=EService-Modbus
+
+if [ ! -d "/home/adv/$ServiceName" ]; then
+        mkdir /home/adv/$ServiceName
+        mkdir /home/adv/$ServiceName/config
+        cp /usr/local/$ServiceName/agent_config.xml /home/adv/$ServiceName/config/.        
+        cp /usr/local/$ServiceName/Modbus_Handler.ini /home/adv/$ServiceName/config/.
+fi
+
+
+if [ ! -d "/home/adv/$ServiceName/config/agent_config.xml" ]; then
+        cp /home/adv/$ServiceName/config/agent_config.xml /usr/local/$ServiceName/.
+fi
+
+if [ ! -d "/home/adv/$ServiceName/config/Modbus_Handler.ini" ]; then
+        cp /home/adv/$ServiceName/config/Modbus_Handler.ini /usr/local/$ServiceName/.
+fi
+
+cd /usr/local/$ServiceName
+
+./cagent
