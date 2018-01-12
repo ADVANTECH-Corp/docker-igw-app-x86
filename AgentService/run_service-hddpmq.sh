4a1c5a28a23a5fde2776a684579962aebcdef9c6
ericliang12345 <ericliang12345@gmail.com>
2018-01-12 10:17:20 +0800

    Update run_service-hddpmq.sh
    [Edge Sense Service] HDDPMQ To remove /run/saagent.pid before running
    
    Date         : 2018/01/12
    Bug #        : N/A
    Description  : To remove /run/saagent.pid before running
    Files        : run_service-hddpmq.sh
    Reviewer     :
    Commit by    : Eric.Liang

--- AgentService/run_service-hddpmq.sh
+++ AgentService/run_service-hddpmq.sh
@@ -13,6 +13,10 @@ if [ ! -d "/home/adv/$ServiceName/config/agent_config.xml" ]; then
         cp /home/adv/$ServiceName/config/agent_config.xml /usr/local/$ServiceName/.
 fi
 
+if [ ! -d "/run/saagent.pid" ]; then
+        rm /run/saagent.pid
+fi
+
 cd /usr/local/$ServiceName
 
 ./cagent
